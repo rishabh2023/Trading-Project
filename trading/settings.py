e80a07d689d30a59c3ac4f331d59c9b9ee9fe926
rishabh2023 <rishabh2023@gmail.com>
2021-07-15 12:56:09 +0530

    final

--- trading/settings.py
+++ trading/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tradingapp',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- tradingapp/urls.py
+++ tradingapp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-   path('api/info/', Tradinginfo.as_view(), name='Get Info'),
+   path('', Tradinginfo.as_view(), name='Get Info'),
 ]
