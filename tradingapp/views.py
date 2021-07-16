04f1b93e7d626f6d6990c50c1ebf14f990129308
rishabh2023 <rishabh2023@gmail.com>
2021-07-16 09:18:30 +0530

    ok

--- tradingapp/views.py
+++ tradingapp/views.py
@@ -7,10 +7,12 @@ from rest_framework import status
 class Tradinginfo(APIView):
     def get(self, request):
         data = request.data
+        print(data)
         return Response(data = data,status = status.HTTP_200_OK)
 
     def post(self, request):
         data = request.data
+        print(data)
         return Response(data = data, status=status.HTTP_200_OK)
 
 
